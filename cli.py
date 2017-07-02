20bb6763c5eb8758a9ddc5e37e9ecb691ce9241f
Volcyy <Johannes.christ@gmail.com>
2017-07-02 14:15:45 +0200

    Move from Shell to argparsing

--- cli.py
+++ cli.py
@@ -1,41 +1,8 @@
 """Handles interacting with the User over a CLI."""
 
 import argparse
-import cmd
-import json
 
-import builder
 import parse
-
-
-class CMSCShell(cmd.Cmd):
-    intro = 'Champion Mains Subscriber Counter'
-    prompt = 'cmsc} '
-
-
-    def do_table(self, _):
-        """
-        Builds a table from the Subreddits specified in
-        config.json. Returns it in Reddit's formatting
-        style for easy copy / paste.
-        """
-
-        print(builder.get_table())
-
-    def do_extract(self, _):
-        """
-        A tool to extract the Subreddit Names from a
-        "reddit table" posted in a file called curr_list.txt.
-        """
-
-        with open('curr_list.txt') as old_list:
-            result = [
-                ''.join(
-                    c for c in l if c.isalpha()
-                ).lstrip('/r/') for l in old_list if 'Rank' not in l
-            ]
-        print(len(result))
-        print(json.dumps(sorted(result), indent=4))
 
 
 def get_args() -> argparse.Namespace:
@@ -49,9 +16,10 @@ def get_args() -> argparse.Namespace:
     )
     parse.add_extractor(subparsers)
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
-    print(get_args())
+    get_args()
 
