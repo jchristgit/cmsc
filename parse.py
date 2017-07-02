994e4826650688c9ec9e7ae8e9cd05b7cd6b9df0
Volcyy <Johannes.christ@gmail.com>
2017-07-02 21:21:16 +0200

    Add build subcommand

--- parse.py
+++ parse.py
@@ -4,13 +4,15 @@ import argparse
 import sys
 import json
 
+import table_builder
+
 
 def handle_extract(args: argparse.Namespace):
     """
     Handles input given to the `extractor` Command.
     """
 
-    if args.include_r:
+    if args.include_r > 0:
         result = [
             ''.join(
                 c for c in l if c.isalpha()
@@ -22,9 +24,21 @@ def handle_extract(args: argparse.Namespace):
                 c for c in l if c.isalpha()
             ).lstrip('/r/') for l in args.infile if 'Rank' not in l
         ]
-    print(result)
-    args.outfile.write(json.dumps(sorted(result), indent=args.indent))
-    args.outfile.write('\n')
+
+    args.outfile.write(
+        json.dumps(
+            sorted(
+                result,
+                key=lambda s: s[0].lower() if s else s
+            ),
+            indent=args.indent
+        )
+    )
+
+    if args.outfile is sys.stdout:
+        args.outfile.write('\n')
+    else:
+        print(f'Wrote extracted Subreddit Names to {args.outfile.name}.')
     args.outfile.close()
 
 
@@ -48,7 +62,7 @@ def add_extractor(subparsers: argparse.ArgumentParser):
     extractor.add_argument(
         '-o', '--outfile',
         help='The file to write results to.',
-        type=argparse.FileType(mode='r'),
+        type=argparse.FileType(mode='w+'),
         default=sys.stdout
     )
     extractor.add_argument(
@@ -61,11 +75,61 @@ def add_extractor(subparsers: argparse.ArgumentParser):
     extractor.add_argument(
         '-r', '--include-r',
         help=('Whether to include the /r/ prepending '
-              'the Subreddit names or not. Defaults to '
+              'the Subreddit names. Defaults to '
               'not including them.'),
-        type=bool,
-        default=False
+        action='count',
+        default=0
     )
     extractor.set_defaults(func=handle_extract)
 
+
+def handle_build(args: argparse.Namespace):
+    """
+    Handles the invocation of the `build` Command.
+    Provides verbosity options.
+    """
+
+    table = table_builder.get_table(args.nocomp > 0)
+
+    args.outfile.write(table)
+    if args.outfile is sys.stdout:
+        args.outfile.write('\n')
+    else:
+        print('Wrote results to specified file.')
+    args.outfile.close()
+
+
+def add_builder(subparsers: argparse.ArgumentParser):
+    """
+    Adds the `build` Command to the parser,
+    which is used to build the Reddit tables.
+    """
+
+    builder = subparsers.add_parser(
+        'build',
+        help='Builds tables to be used in Reddit.'
+    )
+
+    builder.add_argument(
+        '-o', '--outfile',
+        help=('The file to which the table should '
+              'be written. Defaults to printing to '
+              'console.'),
+        type=argparse.FileType(mode='w+'),
+        default=sys.stdout
+    )
+
+    builder.add_argument(
+        '-nc', '--nocomp',
+        help=('If this flag is given, then the '
+              'application will not attempt to compare '
+              'the results of the subscriber counting '
+              'to the results of previous months (and '
+              'thus, not display any differences '
+              'between them in the output'),
+        action='count',
+        default=0
+    )
+    
+    builder.set_defaults(func=handle_build)
 
